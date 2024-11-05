4458194d0cc2bb209b22c8fa9615eef5b20cd4df
RGM <1719501094@qq.com>
2024-11-05 19:39:38 +0800

    init

---
+++ models/Conv/SPDConv.py
@@ -0,0 +1,16 @@
+############## space_to_depth    start  by  AI&CV  AI little monsters #######################################################
+
+import torch
+import torch.nn as nn
+
+
+class space_to_depth(nn.Module):
+    # Changing the dimension of the Tensor
+    def __init__(self, dimension=1):
+        super().__init__()
+        self.d = dimension
+
+    def forward(self, x):
+        return torch.cat([x[..., ::2, ::2], x[..., 1::2, ::2], x[..., ::2, 1::2], x[..., 1::2, 1::2]], 1)
+
+############## space_to_depth    EDN  by  AI&CV  AI little monsters #######################################################
---
+++ models/DEANet.py
@@ -0,0 +1,90 @@
+import torch
+from torch import nn
+from einops import rearrange
+
+class ChannelAttention_CGA(nn.Module):
+    # SegNext NeurIPS 2022
+    # https://github.com/Visual-Attention-Network/SegNeXt/tree/main
+    def __init__(self, dim):
+        super().__init__()
+        self.conv0 = nn.Conv2d(dim, dim, 5, padding=2, groups=dim)
+        self.conv0_1 = nn.Conv2d(dim, dim, (1, 7), padding=(0, 3), groups=dim)
+        self.conv0_2 = nn.Conv2d(dim, dim, (7, 1), padding=(3, 0), groups=dim)
+
+        self.conv1_1 = nn.Conv2d(dim, dim, (1, 11), padding=(0, 5), groups=dim)
+        self.conv1_2 = nn.Conv2d(dim, dim, (11, 1), padding=(5, 0), groups=dim)
+
+        self.conv2_1 = nn.Conv2d(dim, dim, (1, 21), padding=(0, 10), groups=dim)
+        self.conv2_2 = nn.Conv2d(dim, dim, (21, 1), padding=(10, 0), groups=dim)
+        self.conv3 = nn.Conv2d(dim, dim, 1)
+
+    def forward(self, x):
+        u = x.clone()
+        attn = self.conv0(x)
+
+        attn_0 = self.conv0_1(attn)
+        attn_0 = self.conv0_2(attn_0)
+
+        attn_1 = self.conv1_1(attn)
+        attn_1 = self.conv1_2(attn_1)
+
+        attn_2 = self.conv2_1(attn)
+        attn_2 = self.conv2_2(attn_2)
+        attn = attn + attn_0 + attn_1 + attn_2
+
+        attn = self.conv3(attn)
+
+        return attn * u
+
+
+
+class SpatialAttention_CGA(nn.Module):
+    def __init__(self):
+        super(SpatialAttention_CGA, self).__init__()
+        self.sa = nn.Conv2d(2, 1, 7, padding=3, padding_mode='reflect', bias=True)
+
+    def forward(self, x):
+        x_avg = torch.mean(x, dim=1, keepdim=True)
+        x_max, _ = torch.max(x, dim=1, keepdim=True)
+        x2 = torch.concat([x_avg, x_max], dim=1)
+        sattn = self.sa(x2)
+        return sattn
+
+
+
+class PixelAttention_CGA(nn.Module):
+    def __init__(self, dim):
+        super(PixelAttention_CGA, self).__init__()
+        self.pa2 = nn.Conv2d(2 * dim, dim, 7, padding=3, padding_mode='reflect', groups=dim, bias=True)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x, pattn1):
+        B, C, H, W = x.shape
+        x = x.unsqueeze(dim=2)  # B, C, 1, H, W
+        pattn1 = pattn1.unsqueeze(dim=2)  # B, C, 1, H, W
+        x2 = torch.cat([x, pattn1], dim=2)  # B, C, 2, H, W
+        x2 = rearrange(x2, 'b c t h w -> b (c t) h w')
+        pattn2 = self.pa2(x2)
+        pattn2 = self.sigmoid(pattn2)
+        return pattn2
+
+
+class CGAFusion(nn.Module):
+    def __init__(self, dim):
+        super(CGAFusion, self).__init__()
+        self.sa = SpatialAttention_CGA()
+        self.ca = ChannelAttention_CGA(dim)
+        self.pa = PixelAttention_CGA(dim)
+        self.conv = nn.Conv2d(dim, dim, 1, bias=True)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, data):
+        x, y = data
+        initial = x + y
+        cattn = self.ca(initial)
+        sattn = self.sa(initial)
+        pattn1 = sattn + cattn
+        pattn2 = self.sigmoid(self.pa(initial, pattn1))
+        result = initial + pattn2 * x + (1 - pattn2) * y
+        result = self.conv(result)
+        return result
